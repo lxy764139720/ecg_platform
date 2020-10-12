68acc91118595833778eeffeb69f5648ca1738b4
lxy764139720 <764139720@qq.com>
2020-10-12 19:49:09 +0800

    init

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/ecg/index')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('ecg/httptest')
+def httptest():
+    msg = request.args.get('msg') or None
+    signature = request.args.get('signature') or None
+    nonce = request.args.get('nonce') or None
+    if not all([msg, signature, nonce]):
+        return 'invalid parameter'
+    return msg
+
+
+if __name__ == '__main__':
+    app.run()
