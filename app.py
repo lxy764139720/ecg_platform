367c19716408a39c9af4732fe6c0247aa87ecfb2
lxy764139720 <764139720@qq.com>
2020-11-01 12:03:18 +0800

    网页前端跳转

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from models import User
+from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__)  # 创建 Flask 应用
+
+
+# app.secret_key = 'abc'  # 设置表单交互密钥
+# login_manager = LoginManager()  # 实例化登录管理对象
+# login_manager.init_app(app)  # 初始化应用
+# login_manager.login_view = 'login'  # 设置用户登录视图函数 endpoint
+
+
+@app.route('/ecg/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login_register.html')
+
+
+@app.route('/ecg/register', methods=['GET', 'POST'])
+def register():
+    return render_template('index.html')
 
 
 @app.route('/ecg/index')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ecg/chart')
+def chart():
+    return render_template('chart.html')
 
 
 @app.route('/ecg/httptest', methods=['GET', 'POST'])
@@ -23,4 +46,5 @@ def httptest():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
